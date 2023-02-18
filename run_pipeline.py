e9974f1745e46e72f3796eae34df0778ad736d5b
Ilan Aizelman <bebetteryou2015@gmail.com>
2023-02-18 13:03:49 +0200

    Update run_pipeline.py
    handling images

--- run_pipeline.py
+++ run_pipeline.py
@@ -11,6 +11,7 @@ import os
 import time
 from nebula3_reid.facenet_pytorch.pipeline_task.reid_task import test_pipeline_task
 from nebula3_reid.facenet_pytorch.examples.reid_inference_mdf import FaceReId
+from nebula3_llm_task.llm_orchestration import LlmTaskInternal
 
 class InitialPipeline:
     def __init__(self):
@@ -61,16 +62,18 @@ class InitialPipeline:
 
 def main():
     
+    videoprocessing_instance = VideoProcessingExpert()
     pipeline_instance = InitialPipeline()
-    start_time = time.time()
-    pipeline_id = pipeline_instance.init_pipeline('https://saucepankids.com/wp-content/uploads/2019/06/Untitled-design-26.png') #"5b0c75bf-5fd6-4c1e-9f7e-5f51a9a96bd8"
+    pipeline_id = pipeline_instance.init_pipeline('https://d1nslcd7m2225b.cloudfront.net/Pictures/480xany/8/6/3/1388863_fullriverred_499613.jpeg') #"5b0c75bf-5fd6-4c1e-9f7e-5f51a9a96bd8"
     # Necessary for videoprocessing task
     os.environ['ARANGO_HOST'] = "172.83.9.249"
     os.environ['EXPERT_RUN_MODE'] = 'task'
     os.environ['PIPELINE_ID'] = pipeline_id
     
-    videoprocessing_instance = VideoProcessingExpert()
+    start_time = time.time()
     videoprocessing_instance.run_pipeline_task()
+    end_time = time.time() - start_time
+    # print("Video #{} took: {}".format(idx, end_time))
 
     # run reid
     reid_instance = FaceReId()
@@ -86,7 +89,8 @@ def main():
     # run fusion
     fusion_test()
     # run llm
-    llm_test()
+    llm_instance = LlmTaskInternal()
+    llm_test(llm_instance)
     end_time = time.time() - start_time
     print("Total time it took for whole pipeline: {}".format(end_time))
 
@@ -129,22 +133,39 @@ def main():
             pipeline_structure = pipeline_instance.nre.get_pipeline_structure(pipeline_id)
             if pipeline_structure['movies']:
                 movie_id = list(pipeline_structure['movies'].keys())[0]
+
                 # run reid
+                start_time_reid = time.time()
                 test_pipeline_task(pipeline_id, reid_instance)
                 pipeline_instance.update_pipeline_status("reid")
+                end_time2 = time.time() - start_time_reid
+                print("Total time it took for REID: {}".format(end_time2))
+
                 # run visual_clues 
+                start_time_visual_clues = time.time()
                 visual_clues_test(visual_clues_instance)
                 pipeline_instance.update_pipeline_status("visual_clues")
+                end_time2 = time.time() - start_time_visual_clues
+                print("Total time it took for Visual Clues: {}".format(end_time2))
+                
                 # run fusion
+                start_time_fusion = time.time()
                 fusion_test()
                 pipeline_instance.update_pipeline_status("fusion")
+                end_time2 = time.time() - start_time_fusion
+                print("Total time it took for FUSION: {}".format(end_time2))
+                
                 # run llm
-                llm_test()
+                start_time_llm = time.time()
+                llm_test(llm_instance)
                 pipeline_instance.update_pipeline_status("llm")
+                end_time2 = time.time() - start_time_llm
+                print("Total time it took for LLM Task: {}".format(end_time2))
 
                 end_time = time.time() - start_time
                 print("Total time it took for whole pipeline: {}".format(end_time))
 
+            time.sleep(2)
             rc2 = pipeline_instance.nre.get_doc_by_key({'_key': "123456789"}, "pipeline_url")
             cur_url_link = rc2["url_link"]
             pipeline_dict = dict()
@@ -153,8 +174,7 @@ def main():
             pipeline_dict["url_link"] = ""
             pipeline_dict["pipeline_id"] = ""
             pipeline_dict["fetching"] = False
-            pipeline_dict["current_task"] = ""
-            time.sleep(1)
+            pipeline_dict["current_task"] = "done"
             pipeline_instance.nre.write_doc_by_key(doc=pipeline_dict, collection_name="pipeline_url", key_list=['unique_key'])
 
         else:
